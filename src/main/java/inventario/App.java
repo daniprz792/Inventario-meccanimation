b8c27e3e4475ea00bcfaa87d2de228ebd65d910a
Daniel Eduardo Lopez Perez <lopezdelp2007@gmail.com>
2026-09-22 15:08:21 -0500

    CRUD completo de items

--- src/main/java/inventario/App.java
+++ src/main/java/inventario/App.java
@@ -1,6 +1,7 @@
 package inventario;
 
 import inventario.dao.ItemDAO;
+import inventario.model.Item;
 
 // entrada de la app de inventario  //
 public class App {
@@ -8,10 +9,31 @@ public class App {
     public static void main(String[] args) {
 
         // El DAO concentra el acceso a los datos y evita mezclar SQL con el arranque.
+
+        //ItemDAO dao = new ItemDAO();
+
+       // Item item = new Item();
+
         ItemDAO dao = new ItemDAO();
 
-        // Consulta los registros actuales y los muestra en la salida de la consola.
-        dao.listarItems();
+        dao.eliminarItem(4);
+
+
+        // item.setId(4);
+        // item.setNombre("LG 27 pulgadas");
+        // ItemDAO dao = new ItemDAO();
+        // dao.actualizarItem(item);
+
+        //  item.setCodigoInventario("IN0003");
+        // item.setProducto("Monitor");
+        // item.setNombre("LG 24 pulgadas");
+        //ItemDAO dao = new ItemDAO();
+
+        //create 
+        //dao.guardarItem(item);
+
+     // Consulta los registros actuales y los muestra en la salida de la consola.
+    dao.listarItems();
 
     }
-}
+} 
--- src/main/java/inventario/dao/ItemDAO.java
+++ src/main/java/inventario/dao/ItemDAO.java
@@ -1,17 +1,85 @@
 package inventario.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
 import inventario.config.ConexionDB;
+import inventario.model.Item;
 
 //acceso a los datos almacenados en la tabla de items //
 public class ItemDAO {
+    //guardar un item en la bd
+
+    // crud - c - create 
+        public void guardarItem(Item item) {
+
+
+            // insertar un nuevo registro en la tabla de items 
+    String sql = """
+        INSERT INTO items
+        (codigo_inventario, producto, nombre)
+        VALUES (?, ?, ?)
+        """;
+
+        // se abre la conexion solo cuando se necesita ejecutaar la consulta
+    try {
+
+        Connection conexion = ConexionDB.conectar();
+
+        PreparedStatement stmt =
+                conexion.prepareStatement(sql);
+
+        stmt.setString(1, item.getCodigoInventario());
+        stmt.setString(2, item.getProducto());
+        stmt.setString(3, item.getNombre());
+
+        stmt.executeUpdate();
+
+        System.out.println("Producto guardado correctamente");
+
+        conexion.close();
+
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+}
+
+//update 
+    public void actualizarItem(Item item) {
+
+    String sql = """
+        UPDATE items
+        SET nombre = ?
+        WHERE id = ?
+        """;
+
+    try {
+
+        Connection conexion = ConexionDB.conectar();
+
+        PreparedStatement stmt =
+                conexion.prepareStatement(sql);
+
+        stmt.setString(1, item.getNombre());
+        stmt.setInt(2, item.getId());
+
+        stmt.executeUpdate();
+
+        System.out.println("Producto actualizado correctamente");
+
+        conexion.close();
+
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+
+}
 
     // cconsulta todos los items y muestra su codigo de barras y nombre en la consola //
+    //read 
     public void listarItems() {
-
         try {
 
             // se abre la conexion solo cuando se necesita ejecutar la consulta
@@ -43,4 +111,33 @@ public class ItemDAO {
         }
 
     }
+
+    // delete - eliminar algun item de la bd 
+    public void eliminarItem(int id) {
+
+    String sql = """
+        DELETE FROM items
+        WHERE id = ?
+        """;
+
+    try {
+
+        Connection conexion = ConexionDB.conectar();
+
+        PreparedStatement stmt =
+                conexion.prepareStatement(sql);
+
+        stmt.setInt(1, id);
+
+        stmt.executeUpdate();
+
+        System.out.println("Producto eliminado correctamente");
+
+        conexion.close();
+
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+
+}
 }
